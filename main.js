bb848f501ca0195dff4f13adcf83f9f70585ee2a
Shah Paran <web.poran@gmail.com>
2025-08-24 00:37:01 +0600

    this is change for redo

--- main.js
+++ main.js
@@ -2,6 +2,18 @@
 
 document.getElementById('loginButton').addEventListener('click', function(e){
     e.preventDefault();
-    console.log('Login button clicked');
-    console.log(e);
+   const mobileNumber = 123456789;
+   const pinCode =2222;
+    const mobileInput = document.getElementById("mobile_input").value;
+    
+
+    const mobileNimberConverted = parseint(mobileInput);
+    console.log(mobileNimberConverted);
+
+    const pinInput = document.getElementById('Pin').value;
+    const pinConverted = parseint(pinInput);
+    console.log(pinConverted + mobileNimberConverted
+
+
+    );
 });
