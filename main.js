7a7d085cebfa513bcdc8a9f3cd70eafb30f25214
Shah Paran <web.poran@gmail.com>
2025-08-24 02:52:48 +0600

    log page dane

--- main.js
+++ main.js
@@ -1,19 +1,1 @@
 // login button functionality 
-
-document.getElementById('loginButton').addEventListener('click', function(e){
-    e.preventDefault();
-   const mobileNumber = 123456789;
-   const pinCode =2222;
-    const mobileInput = document.getElementById("mobile_input").value;
-    
-
-    const mobileNimberConverted = parseint(mobileInput);
-    console.log(mobileNimberConverted);
-
-    const pinInput = document.getElementById('Pin').value;
-    const pinConverted = parseint(pinInput);
-    console.log(pinConverted + mobileNimberConverted
-
-
-    );
-});
