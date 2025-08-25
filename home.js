23d281960328854d3d8085375d5fd62fad1c87ab
Shah Paran <web.poran@gmail.com>
2025-08-25 15:48:03 +0600

    this is re store I

---
+++ home.js
@@ -0,0 +1,7 @@
+document.getElementById('add-money-btn').addEventListener('click',function(e){
+    e.preventDefault();
+    const bankName = document.getElementById('bank-name').value
+    const AccountNumber = document.getElementById('bank-account-num').value;
+
+
+})
--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-// login button functionality 
+// login button functionality ;
+// This  part is login button functionality ;
+
+
+const logIn = document.getElementById('log-in').addEventListener('click', function(e){
+    e.preventDefault();
+    const mobileNumber =123456789;
+    const PinNumber = 2255;
+
+    // this is input  file 
+
+    const mobileNumberValue = document.getElementById('mobile-number-id').value;
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+
+    const pinNumberValue = document.getElementById('pin-number-id').value;
+     const pinNumberValueConverted = parseInt(pinNumberValue);
+     console.log(mobileNumberValueConverted , pinNumberValueConverted);
+
+
+
+    //  this is logic 
+    if ( mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === PinNumber ){
+        window.location.href="main.html"
+    }else{
+        alert('Invalid Pin or pass');
+    }
+
+   
+
+})
